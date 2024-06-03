475c9d5e23f64dc84b277b7388db336295f0882a
Ваня Зиновчук <zinovchukdfuz2@gmail.com>
2024-06-03 20:28:06 +0300

    Add project files.

---
+++ App/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace App
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_FileName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cb_Disk = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Search = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.FileCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.cb_type = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // tb_FileName
+            // 
+            this.tb_FileName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tb_FileName.Location = new System.Drawing.Point(102, 32);
+            this.tb_FileName.Name = "tb_FileName";
+            this.tb_FileName.Size = new System.Drawing.Size(310, 21);
+            this.tb_FileName.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(217, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "FileName";
+            // 
+            // cb_Disk
+            // 
+            this.cb_Disk.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_Disk.FormattingEnabled = true;
+            this.cb_Disk.Location = new System.Drawing.Point(21, 32);
+            this.cb_Disk.Name = "cb_Disk";
+            this.cb_Disk.Size = new System.Drawing.Size(75, 23);
+            this.cb_Disk.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(39, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Disk";
+            // 
+            // btn_Search
+            // 
+            this.btn_Search.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btn_Search.Location = new System.Drawing.Point(545, 32);
+            this.btn_Search.Name = "btn_Search";
+            this.btn_Search.Size = new System.Drawing.Size(75, 23);
+            this.btn_Search.TabIndex = 4;
+            this.btn_Search.Text = "Search";
+            this.btn_Search.UseVisualStyleBackColor = true;
+            this.btn_Search.Click += new System.EventHandler(this.btn_Search_Click);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FileCol});
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(21, 99);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(599, 339);
+            this.listView1.TabIndex = 5;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // FileCol
+            // 
+            this.FileCol.Text = "Path";
+            this.FileCol.Width = 800;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(21, 70);
+            this.progressBar1.MarqueeAnimationSpeed = 1;
+            this.progressBar1.Maximum = 1000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.RightToLeftLayout = true;
+            this.progressBar1.Size = new System.Drawing.Size(599, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.Style = System.Windows.Forms.ProgressBarStyle.Continuous;
+            this.progressBar1.TabIndex = 6;
+            // 
+            // cb_type
+            // 
+            this.cb_type.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cb_type.FormattingEnabled = true;
+            this.cb_type.Items.AddRange(new object[] {
+            "All",
+            "*.exe",
+            "*.sln",
+            "*.png",
+            "*.jpeg",
+            "*.doc",
+            "*.pdf",
+            "*.txt",
+            "*.zip",
+            "*.rar",
+            "*.mp3",
+            "*.mp4",
+            "*.avi",
+            "*.mkv",
+            "*.wmv",
+            "*.flv",
+            "*.mpeg",
+            "*.html"});
+            this.cb_type.Location = new System.Drawing.Point(418, 32);
+            this.cb_type.Name = "cb_type";
+            this.cb_type.Size = new System.Drawing.Size(121, 23);
+            this.cb_type.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(461, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Type";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(462, 450);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 15);
+            this.label4.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(644, 478);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cb_type);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.btn_Search);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cb_Disk);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_FileName);
+            this.Name = "Form1";
+            this.Text = "App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_FileName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cb_Disk;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Search;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.ColumnHeader FileCol;
+        private System.Windows.Forms.ComboBox cb_type;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ App/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace App
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            cb_Disk.Items.Add("All Disk");
+            cb_Disk.SelectedIndex = 0;
+            foreach (var item in DriveInfo.GetDrives())
+            {
+                cb_Disk.Items.Add(item.Name);
+            }
+
+            cb_type.SelectedIndex = 0;
+        }
+        public static IEnumerable<string> EnumerateAllFilesAndDirectories(string path, string pattern)
+        {
+            IEnumerable<string> dirs = null;
+            IEnumerable<string> files = null;
+            try
+            {
+                dirs = Directory.EnumerateDirectories(path, pattern);
+                files = Directory.EnumerateFiles(path, pattern);
+            }
+            catch { }
+
+            if (dirs != null)
+            {
+                foreach (var dir in dirs)
+                {
+                    yield return dir;
+                }
+            }
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+                    yield return file;
+                }
+            }
+
+            IEnumerable<string> directories = null;
+            try { directories = Directory.EnumerateDirectories(path); }
+            catch { }
+
+            if (directories != null)
+            {
+                foreach (var directory in directories)
+                {
+                    foreach (var res in EnumerateAllFilesAndDirectories(directory, pattern))
+                    {
+                        yield return res;
+                    }
+                }
+            }
+        }
+
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            label4.Text = "";
+
+            string pattern = null;
+
+            if (tb_FileName.Text.Contains("*") || tb_FileName.Text.Contains("?"))
+            {
+                pattern = tb_FileName.Text;
+            }
+            else
+            {
+                if (cb_type.SelectedIndex != 0)
+                {
+                    pattern = $"{tb_FileName.Text}{cb_type.Text}";
+                }
+                else { pattern = $"{tb_FileName.Text}*"; }
+            }
+
+
+            if (cb_Disk.Text == "All Disk")
+            {
+                progressBar1.Value = 0;
+                var drives = DriveInfo.GetDrives();
+                progressBar1.Maximum = drives.Length * 1000;
+
+                foreach (var drive in drives)
+                {
+                    new Thread(() =>
+                    {
+                        foreach (var fi in EnumerateAllFilesAndDirectories(drive.Name, pattern))
+                        {
+                            ListViewItem listViewItem = new ListViewItem(fi);
+                            listView1.Items.Add(listViewItem);
+                        }
+                        lock (progressBar1)
+                        {
+                            progressBar1.Value += 1000;
+                        }
+                        lock (label4)
+                        {
+                            label4.Text = $"Found {listView1.Items.Count} matches";
+                        }
+                    }).Start();
+                }
+            }
+            else
+            {
+                progressBar1.Value = 0;
+                progressBar1.Maximum = 10000;
+                new Thread(() =>
+                {
+                    foreach (var fi in EnumerateAllFilesAndDirectories(cb_Disk.Text, pattern))
+                    {
+                        ListViewItem listViewItem = new ListViewItem(fi);
+                        listView1.Items.Add(listViewItem);
+                    }
+                    progressBar1.Value = 10000;
+                    label4.Text = $"Found {listView1.Items.Count} matches";
+                }).Start();
+            }
+
+        }
+    }
+}
